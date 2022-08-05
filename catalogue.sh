d413336330166c98cdfd7384d2e3575f22a68f97
chinna nani <chinnaaaa225@gmail.com>
2022-08-05 15:37:06 +0530

    catalogue file line adjusted

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,9 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-
 useradd roboshop
 
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+
 cd /home/roboshop
 unzip -o /tmp/catalogue.zip
 mv catalogue-main catalogue
