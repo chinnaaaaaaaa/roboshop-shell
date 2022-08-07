a8ab6a65e5c9a72cda69d7e991754eb3053e9af6
chinna nani <chinnaaaa225@gmail.com>
2022-08-08 00:09:44 +0530

    cart success or failure showing colour print command

--- cart.sh
+++ cart.sh
@@ -1,40 +1,77 @@
 echo instaling nodejs repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
+
 
 echo instaling nodejs
 yum install nodejs -y &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
 
 echo adding application user
 useradd roboshop &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
 
 echo downloading the applicant content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>> /tmp/cart.log
 cd /home/roboshop &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
 
 echo cleaning old application
 rm -rf cart &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
 
 echo extrat application archive
 unzip -o /tmp/cart.zip &>> /tmp/cart.log
 mv cart-main cart &>> /tmp/cart.log
 cd cart &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
 
 echo instaling nodejs dependencies
 npm install &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
 
 echo confirouing cart systemD service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
 
 echo starting cart services
 systemctl daemon-reload &>> /tmp/cart.log
 systemctl start cart &>> /tmp/cart.log
 systemctl enable cart &>> /tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+  else
+  echo -e "\e[31mFAILURE\e[0m"
+ fi
