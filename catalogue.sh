c2714b5016673c9618b372a006a4c6afc64e4576
chinna nani <chinnaaaa225@gmail.com>
2022-08-08 19:48:26 +0530

    using common.sh file to easy of scripting

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,6 @@
-set -e
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
+soure common.sh
+
+NODEJS
 
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
 
--- common.sh
+++ common.sh
@@ -8,3 +8,20 @@ StatusCheck() {
     exit 1
   fi
  }
+
+NODEJS() {
+ echo instaling nodejs repos
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/cart.log
+ StatusCheck
+
+ echo instaling nodejs
+  yum install nodejs -y &>> /tmp/cart.log
+ StatusCheck
+
+ id roboshop &>>/tmp/cart.log
+ if [ $? -ne 0 ]; then
+     echo adding application user
+     useradd roboshop &>> /tmp/cart.log
+     StatusCheck
+ fi
+ }
