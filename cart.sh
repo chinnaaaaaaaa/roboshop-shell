0883b163fd4de1ff567bd4e9f96196eab0759f32
chinna nani <chinnaaaa225@gmail.com>
2022-08-08 20:19:42 +0530

    using common.sh file to easy of scripting

--- cart.sh
+++ cart.sh
@@ -1,23 +1,9 @@
 source common.sh
 
+COMPONENT=cart
 NODEJS
 
-echo downloading the applicant content
-curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>> /tmp/cart.log
-cd /home/roboshop &>> /tmp/cart.log
-StatusCheck
 
-echo cleaning old application
-rm -rf cart &>> /tmp/cart.log
-StatusCheck
-
-echo extrat application archive
-unzip -o /tmp/cart.zip &>> /tmp/cart.log && mv cart-main cart &>> /tmp/cart.log && cd cart &>> /tmp/cart.log
-StatusCheck
-
-echo instaling nodejs dependencies
-npm install &>> /tmp/cart.log
-StatusCheck
 
 echo confirouing cart systemD service
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>> /tmp/cart.log && systemctl daemon-reload &>> /tmp/cart.log
