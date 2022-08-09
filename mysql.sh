493f9f02163cbd73fd7c8c6fe824a53a954c8437
chinna nani <chinnaaaa225@gmail.com>
2022-08-09 16:15:30 +0530

    using common.sh file to easily scripting of frontend.sh

--- mysql.sh
+++ mysql.sh
@@ -1,15 +1,23 @@
-set -e
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+source common.sh
+COMOPNENT=mysql
 
-yum install mysql-community-server -y
+echo Set up YUM repos
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+StatusCheck
 
-systemctl enable mysqld
-systemctl start mysqld
+echo install MYSQL
+yum install mysql-community-server -y &>>${LOG}
+StatusCheck
 
-echo MYSQL PASSWORD = $MYSQL_PASSWORD
+echo start MYSQL sevice
+systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
+StatusCheck
+
 DEFAULT_PASSWORD=$( grep 'A temporary password'  /var/log/mysqld.log | awk '{print $NF}')
 
 echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
+
+exit
 
 
 echo  "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD
