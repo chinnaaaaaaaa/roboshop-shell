c1e97a42d4e4cac99fbe2a4d9c1cad26eed7e74c
chinna nani <chinnaaaa225@gmail.com>
2022-08-09 13:23:17 +0530

    using common.sh file to easily scripting of frontend.sh

--- common.sh
+++ common.sh
@@ -9,13 +9,18 @@ StatusCheck() {
   fi
  }
 
+DOWNLOAD() {
+ echo downloading ${COMPONENT} applicant content
+ curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> /tmp/${COMPONENT}.log
+  }
+
 NODEJS() {
  echo instaling nodejs repos
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/${COMPONENT}.log
  StatusCheck
 
  echo instaling nodejs
-  yum install nodejs -y &>> /tmp/${COMPONENT}.log
+ yum install nodejs -y &>> /tmp/${COMPONENT}.log
  StatusCheck
 
  id roboshop &>>/tmp/${COMPONENT}.log
@@ -25,30 +30,27 @@ NODEJS() {
      StatusCheck
  fi
 
-echo downloading the applicant content
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> /tmp/${COMPONENT}.log
-cd /home/roboshop &>> /tmp/${COMPONENT}.log
-StatusCheck
+DOWNLOAD
 
-echo cleaning old application
-rm -rf ${COMPONENT} &>> /tmp/${COMPONENT}.log
-StatusCheck
+ echo cleaning old application
+ cd /home/roboshop &>> /tmp/${COMPONENT}.log && rm -rf ${COMPONENT} &>> /tmp/${COMPONENT}.log
+ StatusCheck
 
-echo extrat application archive
-unzip -o /tmp/${COMPONENT}.zip &>> /tmp/${COMPONENT}.log && mv ${COMPONENT}-main ${COMPONENT} &>> /tmp/${COMPONENT}.log && cd ${COMPONENT} &>> /tmp/${COMPONENT}.log
-StatusCheck
+ echo extrat application archive
+ unzip -o /tmp/${COMPONENT}.zip &>> /tmp/${COMPONENT}.log && mv ${COMPONENT}-main ${COMPONENT} &>> /tmp/${COMPONENT}.log && cd ${COMPONENT} &>> /tmp/${COMPONENT}.log
+ StatusCheck
 
-echo instaling nodejs dependencies
-npm install &>> /tmp/${COMPONENT}.log
-StatusCheck
+ echo instaling nodejs dependencies
+ npm install &>> /tmp/${COMPONENT}.log
+ StatusCheck
 
-echo confirouing ${COMPONENT} systemD service
-mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> /tmp/${COMPONENT}.log && systemctl daemon-reload &>> /tmp/${COMPONENT}.log
-StatusCheck
+ echo confirouing ${COMPONENT} systemD service
+ mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> /tmp/${COMPONENT}.log && systemctl daemon-reload &>> /tmp/${COMPONENT}.log
+ StatusCheck
 
-echo starting ${COMPONENT} services
-systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log && systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
-StatusCheck
+ echo starting ${COMPONENT} services
+ systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log && systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
+ StatusCheck
  }
 
 USER_ID=$( id -u )
@@ -56,3 +58,5 @@ if [ $USER_ID -ne 0 ]; then
   echo -e "\e[31m you should run this script as root user or sudo \e[0m"
   exit 1
 fi
+LOG=/tmp/${COMPONENT}.log
+rm -f ${LOG}
