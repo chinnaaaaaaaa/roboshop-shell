3ccc5c652bc193d44442bd5ffaac25edb38bf40a
chinna nani <chinnaaaa225@gmail.com>
2022-08-09 17:42:09 +0530

    using common.sh file to change mysql password change

--- mysql.sh
+++ mysql.sh
@@ -18,10 +18,13 @@ echo start MYSQL sevice
 systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
 StatusCheck
 
-DEFAULT_PASSWORD=$( grep 'A temporary password'  /var/log/mysqld.log | awk '{print $NF}')
-
-echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
-
+echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>${LOG}
+if [ $? -ne 0 ]; then
+   echo Changing Default Password
+   DEFAULT_PASSWORD=$( grep 'A temporary password'  /var/log/mysqld.log | awk '{print $NF}')
+   echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
+ StatusCheck
+fi
 exit
 
 
