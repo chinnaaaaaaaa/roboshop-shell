e73368697e7dd58d0528e291fa166c331696bb03
chinna nani <chinnaaaa225@gmail.com>
2022-08-09 12:33:02 +0530

    using common.sh file to easy of scripting

--- common.sh
+++ common.sh
@@ -50,3 +50,9 @@ echo starting ${COMPONENT} services
 systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log && systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
 StatusCheck
  }
+
+USER_ID=$( id -u )
+if [ $USER_ID -ne 0 ]; then
+  echo -e "\e[31m you should run this script as root user or sudo \e[0m"
+  exit 1
+fi
