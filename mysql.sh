0bcd792d6f7db39f5619bc45205c18a984e6152e
chinna nani <chinnaaaa225@gmail.com>
2022-08-10 19:27:22 +0530

    using function mysql file running

--- mysql.sh
+++ mysql.sh
@@ -2,8 +2,10 @@ source common.sh
 
 COMPONENT=mysql
 
-
-
+ if [ -z "$MYSQL_PSWWORD" ]; then
+  echo -e "\e[33m env variable MYSQL_PASSWORD missing \e[0m"
+  exit 1
+ fi
 
 echo set YUM repos
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
@@ -18,5 +20,5 @@ systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
 StatusCheck
 
 DEFAULT_PASSWORD=$(grep  'A temporary password' /var/log/mysqld.log |awk '{print $NF}')
-echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';"|  mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
+echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PSWWORD';"|  mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD}
 
