e59064a780e90eeac4d1594c4df0739aaaa61677
chinna nani <chinnaaaa225@gmail.com>
2022-08-10 12:45:51 +0530

    using function mysql file running

--- mysql.sh
+++ mysql.sh
@@ -19,13 +19,9 @@ echo start MYSQL sevice
 systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
 StatusCheck
 
-echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>${LOG}
-if [ $? -ne 0 ]; then
-   echo Changing Default Password
-   DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-   echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD} &>>${LOG}
- StatusCheck
-fi
+
+exit
+
 
 echo  "show plugins;" | mysql -uroot -p$MYSQL_PASSWORD 2>&1 | grep validate_password &>>${LOG}
 if [ $? -eq 0 ]; then
