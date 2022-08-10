f5dd8bfaef324545f26a75f2af342fa849e547c4
chinna nani <chinnaaaa225@gmail.com>
2022-08-10 15:48:54 +0530

    using function mysql file running

--- mysql.sh
+++ mysql.sh
@@ -13,3 +13,6 @@ StatusCheck
 echo start mysql servive
 systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
 StatusCheck
+
+DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';" | mysql -uroot -p${DEFAULT_PASSWORD}
