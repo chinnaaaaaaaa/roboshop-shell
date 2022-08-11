72ef61caaba377bbb5ed61166a41df84f04f4b22
chinna nani <chinnaaaa225@gmail.com>
2022-08-11 14:32:14 +0530

    using shell script excute the rabiitmq file

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,18 +8,18 @@ fi
 
 echo Setup yum repos
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG}
-StatuCheck
+StatusCheck
 
 echo "Instal RabbitMQ & Erlang"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y  &>>${LOG}
-StatuCheck
+StatusCheck
 
 echo Start RabbitMQ Service
 systemctl enable rabbitmq-server &>>${LOG} && systemctl start rabbitmq-server &>>${LOG}
-StatuCheck
+StatusCheck
 
 echo Add App User in RabbitMQ
 rabbitmqctl add_user roboshop ${APP_RABBITMQ_PASSWORD} &>>${LOG} && rabbitmqctl set_user_tags roboshop administrator &>>${LOG} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${LOG}
- StatuCheck
+ StatusCheck
 
 
