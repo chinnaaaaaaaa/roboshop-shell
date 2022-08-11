bc400c0548ae3ffe74eef2e6632cc5527354fe40
chinna nani <chinnaaaa225@gmail.com>
2022-08-11 12:40:10 +0530

    using shell script excute the shipping file

--- common.sh
+++ common.sh
@@ -11,54 +11,79 @@ StatusCheck() {
 
 DOWNLOAD() {
  echo downloading ${COMPONENT} applicant content
- curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> /tmp/${COMPONENT}.log
+ curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>> ${LOG}
+ StatusCheck
+ }
+ APP_USER_SETUP() {
+id roboshop &>>${LOG}
+ if [ $? -ne 0 ]; then
+     echo adding application user
+     useradd roboshop &>> ${LOG}
+     StatusCheck
+ fi
+ APP_CLEAN() {
+ echo cleaning old application
+ cd /home/roboshop &>> ${LOG} && rm -rf ${COMPONENT} &>> ${LOG}
+ StatusCheck
+
+ echo extrat application archive
+ unzip -o /tmp/${COMPONENT}.zip &>> ${LOG} && mv ${COMPONENT}-main ${COMPONENT} &>> ${LOG} && cd ${COMPONENT} &>> ${LOG}
+ StatusCheck
+}
+SYSTEMD() {
+echo confirouing ${COMPONENT} systemD service
+ mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> ${LOG} && systemctl daemon-reload &>> ${LOG}
+ StatusCheck
+
+ echo starting ${COMPONENT} services
+ systemctl start ${COMPONENT} &>> ${LOG} && systemctl enable ${COMPONENT} &>> ${LOG}
  StatusCheck
  }
 
 NODEJS() {
  echo instaling nodejs repos
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/${COMPONENT}.log
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> ${LOG}
  StatusCheck
 
  echo instaling nodejs
- yum install nodejs -y &>> /tmp/${COMPONENT}.log
+ yum install nodejs -y &>> ${LOG}
  StatusCheck
 
- id roboshop &>>/tmp/${COMPONENT}.log
- if [ $? -ne 0 ]; then
-     echo adding application user
-     useradd roboshop &>> /tmp/${COMPONENT}.log
-     StatusCheck
- fi
-
-DOWNLOAD
-
- echo cleaning old application
- cd /home/roboshop &>> /tmp/${COMPONENT}.log && rm -rf ${COMPONENT} &>> /tmp/${COMPONENT}.log
- StatusCheck
-
- echo extrat application archive
- unzip -o /tmp/${COMPONENT}.zip &>> /tmp/${COMPONENT}.log && mv ${COMPONENT}-main ${COMPONENT} &>> /tmp/${COMPONENT}.log && cd ${COMPONENT} &>> /tmp/${COMPONENT}.log
- StatusCheck
+ APP_USER_SETUP
+ DOWNLOAD
+ APP_CLEAN
 
  echo instaling nodejs dependencies
- npm install &>> /tmp/${COMPONENT}.log
+ npm install &>> ${LOG}
  StatusCheck
 
-
- echo confirouing ${COMPONENT} systemD service
- mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>> /tmp/${COMPONENT}.log && systemctl daemon-reload &>> /tmp/${COMPONENT}.log
- StatusCheck
-
- echo starting ${COMPONENT} services
- systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log && systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
- StatusCheck
+ SYSTEMD
  }
+
+ JAVA() {
+   echo Install maven
+   yum install maven -y &>>${LOG}
+   StatusCheck
+
+   APP_USER_SETUP
+   DOWNLOAD
+   APP_CLEAN
+
+ echo Make application package
+   mvn clean package &>>${LOG} && mv target/shipping-1.0.jar shipping.jar &>>${LOG}
+ StatusCheck
+
+
+  SYSTEMD
+
+   }
+
 
 USER_ID=$( id -u )
 if [ $USER_ID -ne 0 ]; then
   echo -e "\e[31m you should run this script as root user or sudo \e[0m"
   exit 1
 fi
-LOG=/tmp/${COMPONENT}.log
+LOG=${LOG}
 rm -f ${LOG}
+}
--- shipping.sh
+++ shipping.sh
@@ -2,23 +2,4 @@ source common.sh
 
 COMPONENT=shipping
 
-echo Install maven
-yum install maven -y &>>${LOG}
-StatusCheck
-
-
-useradd roboshop
-
-cd /home/roboshop
-rm -rf shipping
-curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
-unzip -o /tmp/shipping.zip
-mv shipping-main shipping
-cd shipping
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
-systemctl daemon-reload
-systemctl start shipping
-systemctl enable shipping
+JAVA
