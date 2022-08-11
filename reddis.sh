6aa716a5f169fa7ac14abe6223dfdfe95f9d4d52
chinna nani <chinnaaaa225@gmail.com>
2022-08-11 22:41:02 +0530

    using shell script excute the reddis.sh file

--- reddis.sh
+++ reddis.sh
@@ -10,7 +10,9 @@ echo insatall YUM repo
 yum install redis-6.2.7 -y &>>${LOG}
 StatusCheck
 
-#Update listen IP
+echo Update Redis Listen Address
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+StatusCheck
 
 echo start reddis service
 systemctl enable redis &>>${LOG} && systemctl start redis &>>${LOG}
