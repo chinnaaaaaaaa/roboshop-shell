93d648bbd6f0a4096d25bef2ea5622107c3c2a39
chinna nani <chinnaaaa225@gmail.com>
2022-08-11 21:20:23 +0530

    using shell script excute the mongodb.sh file

--- mongodb.sh
+++ mongodb.sh
@@ -23,6 +23,6 @@ cd /tmp unzip -o mongodb.zip &>>${LOG}
 StatusCheck
 
 echo Load Schema
-cd mongodb-main && mongo < catalogue.js &>>${LOG} && mongo < users.js &>>${LOG}
+cd mongodb-main &>>${LOG} && mongo < catalogue.js &>>${LOG} && mongo < users.js &>>${LOG}
 StatusCheck
 
