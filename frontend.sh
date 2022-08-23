c71e880fb82a6657cbf4d936ef71cbbba6d441ea
chinna nani <chinnaaaa225@gmail.com>
2022-08-23 23:20:10 +0530

    using shell script update the Nginx cofiguration files

--- frontend.sh
+++ frontend.sh
@@ -17,7 +17,7 @@ unzip -o /tmp/frontend.zip &>>${LOG} && mv frontend-main/static/* . && mv fronte
 StatusCheck
 
 echo Updating Nginx Configuration
-sed -i -e '/catalogue/ s/localhost/catalogue-dev.roboshop.internal/'  -e '/cart/ s/localhost/cart-dev.roboshop.internal/'  -e '/user/ s/localhost/user-dev.roboshop.internal/'  -e '/payment/ s/localhost/payment-dev.roboshop.internal/'  -e'/shipping/ s/localhost/shipping-dev.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue-dev.roboshop.internal/' -e '/cart/ s/localhost/cart-dev.roboshop.internal/' -e '/user/ s/localhost/user-dev.roboshop.internal/' -e '/payment/ s/localhost/payment-dev.roboshop.internal/' -e'/shipping/ s/localhost/shipping-dev.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 StatusCheck
 
 echo Start Nginx Service
